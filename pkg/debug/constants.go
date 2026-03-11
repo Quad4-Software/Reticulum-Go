3adb2a03335fd0234d77a190f07602874707bd46
Sudo-Ivan <ivan@quad4.io>
2026-03-10 23:25:09 -0500

    feat(debug): add debug log level constants for improved logging verbosity

---
+++ pkg/debug/constants.go
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: 0BSD
+// Copyright (c) 2024-2026 Sudo-Ivan / Quad4.io
+package debug
+
+// Debug log levels (higher = more verbose).
+const (
+	DEBUG_CRITICAL = 1
+	DEBUG_ERROR    = 2
+	DEBUG_INFO     = 3
+	DEBUG_VERBOSE  = 4
+	DEBUG_TRACE    = 5
+	DEBUG_PACKETS  = 6
+	DEBUG_ALL      = 7
+)
