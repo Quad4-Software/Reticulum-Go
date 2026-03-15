c536ca5bf5d0baea52031db04b188478e3c2712c
Ivan <ivan@quad4.io>
2026-03-15 14:46:02 +0300

    test(link): add responder callback timing test to validate link establishment

---
+++ pkg/link/responder_test.go
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: 0BSD
+// Copyright (c) 2024-2026 Sudo-Ivan / Quad4.io
+package link
+
+import (
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"git.quad4.io/Networks/Reticulum-Go/pkg/common"
+	"git.quad4.io/Networks/Reticulum-Go/pkg/destination"
+	"git.quad4.io/Networks/Reticulum-Go/pkg/identity"
+	"git.quad4.io/Networks/Reticulum-Go/pkg/transport"
+)
+
+func TestResponderCallbackTiming(t *testing.T) {
+	// Create Node A (Server/Responder)
+	cfgA := &common.ReticulumConfig{}
+	trA := transport.NewTransport(cfgA)
+	defer trA.Close()
+	idA, _ := identity.New()
+
+	// Create Node B (Client/Initiator)
+	cfgB := &common.ReticulumConfig{}
+	trB := transport.NewTransport(cfgB)
+	defer trB.Close()
+	idB, _ := identity.New()
+	_ = idB
+
+	// Connect them via PipeInterface
+	pipeA := NewPipeInterface("pipeA")
+	pipeB := NewPipeInterface("pipeB")
+	pipeA.peer = pipeB
+	pipeB.peer = pipeA
+	pipeA.tr = trA
+	pipeB.tr = trB
+
+	_ = trA.RegisterInterface("pipeA", pipeA)
+	_ = trB.RegisterInterface("pipeB", pipeB)
+
+	// Setup Destination on Node A
+	destA, _ := destination.New(idA, destination.IN, destination.SINGLE, "testapp", trA, "service")
+	destA.AcceptsLinks(true)
+
+	var callbackTriggered atomic.Bool
+	var linkStatusAtCallback atomic.Int32
+
+	destA.SetLinkEstablishedCallback(func(l interface{}) {
+		lnk, ok := l.(*Link)
+		if ok {
+			linkStatusAtCallback.Store(lnk.status.Load())
+			callbackTriggered.Store(true)
+		}
+	})
+
+	// Node A announces itself
+	_ = destA.Announce(false, nil, nil)
+	time.Sleep(100 * time.Millisecond)
+
+	// Node B establishes a link to Node A
+	linkB := NewLink(destA, trB, pipeB, nil, nil)
+	if err := linkB.Establish(); err != nil {
+		t.Fatalf("Link establishment failed: %v", err)
+	}
+
+	// Wait for establishment with a timeout
+	timeout := time.After(2 * time.Second)
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			t.Fatal("Timed out waiting for link establishment")
+		case <-ticker.C:
+			if callbackTriggered.Load() {
+				status := byte(linkStatusAtCallback.Load())
+				if status != STATUS_ACTIVE {
+					t.Errorf("Callback triggered with invalid status: %d (expected %d/STATUS_ACTIVE)", status, STATUS_ACTIVE)
+				}
+				return
+			}
+		}
+	}
+}
