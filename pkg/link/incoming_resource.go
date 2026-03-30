ac2779d18799b5c99cc184638086ae885288eb54
Ivan <ivan@quad4.io>
2026-03-30 17:30:38 +0300

    feat(link): improve incoming resource management with hashmap segment handling and state updates

--- pkg/link/incoming_resource.go
+++ pkg/link/incoming_resource.go
@@ -7,6 +7,7 @@ import (
 	"crypto/sha256"
 	"errors"
 	"io"
+	"math"
 
 	"git.quad4.io/Go-Libs/bzip2/pkg/bzip2"
 	"git.quad4.io/Networks/Reticulum-Go/pkg/packet"
@@ -15,6 +16,7 @@ import (
 
 const (
 	hashmapNotExhausted = 0x00
+	hashmapExhausted    = 0xff
 )
 
 type incomingResourceAsm struct {
@@ -22,9 +24,30 @@ type incomingResourceAsm struct {
 	sdu int
 
 	partSlots            [][]byte
-	hashmapFlat          []byte
+	mapHashes            [][]byte
+	hashmapHeight        int
 	totalParts           int
 	consecutiveCompleted int
+	waitingForHmu        bool
+}
+
+func (rx *incomingResourceAsm) applyHashmapSegment(segment int, hashmapBytes []byte) {
+	segLen := resource.HashmapEntriesPerSegment(rx.sdu)
+	if segLen <= 0 {
+		segLen = 1
+	}
+	hashes := len(hashmapBytes) / resource.MAPHASH_LEN
+	for i := 0; i < hashes; i++ {
+		idx := i + segment*segLen
+		if idx >= rx.totalParts {
+			break
+		}
+		if rx.mapHashes[idx] == nil {
+			rx.hashmapHeight++
+		}
+		off := i * resource.MAPHASH_LEN
+		rx.mapHashes[idx] = append([]byte(nil), hashmapBytes[off:off+resource.MAPHASH_LEN]...)
+	}
 }
 
 func (l *Link) beginIncomingResource(adv *resource.ResourceAdvertisement) error {
@@ -37,20 +60,22 @@ func (l *Link) beginIncomingResource(adv *resource.ResourceAdvertisement) error 
 	if adv.Parts <= 0 {
 		return errors.New("invalid parts in advertisement")
 	}
-	need := adv.Parts * resource.MAPHASH_LEN
-	if len(adv.Hashmap) < need {
-		return errors.New("advertisement hashmap shorter than parts")
+	if len(adv.Hashmap) == 0 || len(adv.Hashmap)%resource.MAPHASH_LEN != 0 {
+		return errors.New("invalid advertisement hashmap")
 	}
 
-	l.incomingMu.Lock()
-	l.incomingRx = &incomingResourceAsm{
+	rx := &incomingResourceAsm{
 		adv:                  adv,
 		sdu:                  sdu,
 		partSlots:            make([][]byte, adv.Parts),
-		hashmapFlat:          append([]byte(nil), adv.Hashmap[:need]...),
+		mapHashes:            make([][]byte, adv.Parts),
 		totalParts:           adv.Parts,
 		consecutiveCompleted: -1,
 	}
+	rx.applyHashmapSegment(0, adv.Hashmap)
+
+	l.incomingMu.Lock()
+	l.incomingRx = rx
 	l.incomingMu.Unlock()
 	return l.sendIncomingResourceReqNext()
 }
@@ -59,6 +84,10 @@ func (l *Link) sendIncomingResourceReqNext() error {
 	l.incomingMu.Lock()
 	rx := l.incomingRx
 	if rx == nil {
+		l.incomingMu.Unlock()
+		return nil
+	}
+	if rx.waitingForHmu {
 		l.incomingMu.Unlock()
 		return nil
 	}
@@ -78,29 +107,49 @@ func (l *Link) sendIncomingResourceReqNext() error {
 	}
 
 	var requestedHashes []byte
+	exhausted := false
 	batch := 0
-	for j := searchStart; j < end; j++ {
-		if rx.partSlots[j] == nil {
-			off := j * resource.MAPHASH_LEN
-			if off+resource.MAPHASH_LEN > len(rx.hashmapFlat) {
-				l.incomingMu.Unlock()
-				return errors.New("incoming resource hashmap underrun")
-			}
-			mh := rx.hashmapFlat[off : off+resource.MAPHASH_LEN]
+	for pn := searchStart; pn < end; pn++ {
+		if rx.partSlots[pn] != nil {
+			continue
+		}
+		mh := rx.mapHashes[pn]
+		if mh != nil {
 			requestedHashes = append(requestedHashes, mh...)
 			batch++
 			if batch >= resource.WINDOW {
 				break
 			}
+			continue
 		}
+		exhausted = true
+		break
 	}
 
-	if len(requestedHashes) == 0 {
+	if len(requestedHashes) == 0 && !exhausted {
 		l.incomingMu.Unlock()
 		return nil
 	}
 
-	reqBody := append(append([]byte{hashmapNotExhausted}, rx.adv.Hash...), requestedHashes...)
+	var prefix []byte
+	if exhausted {
+		if rx.hashmapHeight == 0 || rx.hashmapHeight-1 >= len(rx.mapHashes) {
+			l.incomingMu.Unlock()
+			return errors.New("incoming resource cannot request hashmap extension")
+		}
+		last := rx.mapHashes[rx.hashmapHeight-1]
+		if len(last) != resource.MAPHASH_LEN {
+			l.incomingMu.Unlock()
+			return errors.New("invalid last map hash for HMU request")
+		}
+		prefix = append([]byte{hashmapExhausted}, last...)
+		rx.waitingForHmu = true
+	} else {
+		prefix = []byte{hashmapNotExhausted}
+	}
+
+	reqBody := append(prefix, rx.adv.Hash...)
+	reqBody = append(reqBody, requestedHashes...)
 	l.incomingMu.Unlock()
 
 	return l.SendPacketWithContext(reqBody, packet.ContextResourceReq)
@@ -110,6 +159,19 @@ func (l *Link) resetIncomingResource() {
 	l.incomingMu.Lock()
 	l.incomingRx = nil
 	l.incomingMu.Unlock()
+}
+
+func (l *Link) applyIncomingHashmapUpdate(resHash []byte, segment int, hashmapBytes []byte) error {
+	l.incomingMu.Lock()
+	rx := l.incomingRx
+	if rx == nil || rx.adv == nil || !bytes.Equal(resHash, rx.adv.Hash) {
+		l.incomingMu.Unlock()
+		return nil
+	}
+	rx.applyHashmapSegment(segment, hashmapBytes)
+	rx.waitingForHmu = false
+	l.incomingMu.Unlock()
+	return l.sendIncomingResourceReqNext()
 }
 
 func (l *Link) appendIncomingResourcePart(data []byte) error {
@@ -142,11 +204,13 @@ func (l *Link) appendIncomingResourcePart(data []byte) error {
 
 	idx := -1
 	for i := 0; i < rx.totalParts; i++ {
-		off := i * resource.MAPHASH_LEN
-		if off+resource.MAPHASH_LEN > len(rx.hashmapFlat) {
-			break
+		if rx.partSlots[i] != nil {
+			continue
 		}
-		if bytes.Equal(rx.hashmapFlat[off:off+resource.MAPHASH_LEN], mh) {
+		if len(rx.mapHashes[i]) != resource.MAPHASH_LEN {
+			continue
+		}
+		if bytes.Equal(rx.mapHashes[i], mh) {
 			idx = i
 			break
 		}
@@ -247,4 +311,38 @@ func (l *Link) assembleIncomingPayload(inner []byte, adv *resource.ResourceAdver
 	}
 
 	return data, nil
+}
+
+func wireInt(v any) (int, bool) {
+	switch x := v.(type) {
+	case int:
+		return x, true
+	case int8:
+		return int(x), true
+	case int16:
+		return int(x), true
+	case int32:
+		return int(x), true
+	case int64:
+		if x > int64(math.MaxInt) || x < int64(math.MinInt) {
+			return 0, false
+		}
+		return int(x), true
+	case uint8:
+		return int(x), true
+	case uint16:
+		return int(x), true
+	case uint32:
+		if int64(x) > int64(math.MaxInt) {
+			return 0, false
+		}
+		return int(x), true
+	case uint64:
+		if x > uint64(math.MaxInt) {
+			return 0, false
+		}
+		return int(x), true
+	default:
+		return 0, false
+	}
 }
