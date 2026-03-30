07d7cb2bf0da97a77d151e57d4d39f134f634272
Ivan <ivan@quad4.io>
2026-03-30 15:29:52 +0300

    feat(resource): add bzip2 compression function for byte slices

---
+++ pkg/resource/bzip2_compress.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: 0BSD
+// Copyright (c) 2024-2026 Sudo-Ivan / Quad4.io
+package resource
+
+import (
+	"bytes"
+
+	"git.quad4.io/Go-Libs/bzip2/pkg/bzip2"
+)
+
+func bzip2CompressBody(in []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w, err := bzip2.NewWriter(&buf, 6)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := w.Write(in); err != nil {
+		return nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
